bbce500503388252a9a5de5ed08dfc927300ecfb
Djalma Silva <djlmasilva455@gmail.com>
2022-09-22 11:35:10 +0100

    Commit Inicial

---
+++ script.js
@@ -0,0 +1,6 @@
+
+    function calculo(){
+    var temp = document.querySelector("#temperatura").value;
+    var result =(temp*1.8)+ 32;
+    alert(' O Resultado da conversão é:' + result +  'Fahrenheit');
+}
